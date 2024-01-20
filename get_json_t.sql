d5d14595e4b2a9faf7bffac1e7db517d57ab52f7
Arnaud <arnaud.schouten+git@gmail.com>
2024-01-20 13:46:05 +0100

    handling of date columns

--- get_json_t.sql
+++ get_json_t.sql
@@ -9,6 +9,7 @@ create or replace function get_json_t (
 , p_exclude_cols in dbms_tf.columns_t default null
 , p_hide_null_values in boolean default true
 , p_json_column in string_type default null
+, p_date_columns dbms_tf.columns_t default null
 ) return clob sql_macro ( table ) is
   l_column_name    varchar2(32767);
   type key_value_rectype is record (
@@ -18,7 +19,7 @@ create or replace function get_json_t (
   type key_value_tabtype is
     table of key_value_rectype index by pls_integer;
   l_key_value_list key_value_tabtype;
-  l_stmt           varchar2(32767);
+  l_stmt           clob;
   j                pls_integer := 1;
 begin
   for i in 1..p_tab.column.count loop
@@ -27,11 +28,18 @@ begin
       l_column_name := trim(both '"' from p_tab.column(i).description.name);
       l_key_value_list(j).k := substr(lower(l_column_name), 1, 1)
                                || substr(replace(initcap(l_column_name), '_'), 2);
-      if ( i < p_tab.column.count ) then
+      if ( p_date_columns is not null
+      and p_tab.column(i).description.name member of p_date_columns ) then
+        l_key_value_list(j).v := 'to_char('
+                                 || p_tab.column(i).description.name
+                                 || ', ''yyyy-mm-dd''),';
+      else
         l_key_value_list(j).v := p_tab.column(i).description.name
                                                || ',';
-      else
-        l_key_value_list(j).v := p_tab.column(i).description.name;
+      end if;
+
+      if ( i = p_tab.column.count ) then
+        l_key_value_list(j).v := rtrim(l_key_value_list(j).v, ',');
       end if;
 
       j := j + 1;
