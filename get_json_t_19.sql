227372ea6d7000fcda897892d1caa14dc38c4d08
Arnaud <100875450+ArnaudSchouten@users.noreply.github.com>
2024-01-22 12:00:39 +0100

    Update get_json_t_19.sql
    boolean support

--- get_json_t_19.sql
+++ get_json_t_19.sql
@@ -1,17 +1,29 @@
 create or replace function get_json_t
+/* vb
+select t.id zzp_id
+      ,t.document
+  from get_json_t(p_tab              => b2b_zzp
+                 ,p_exclude_cols     => columns(aangemaakt_door, gewijzigd_door, datum_aangemaakt, datum_gewijzigd)
+                 ,p_date_columns     => columns(geboorte_datum)
+                 ,p_boolean_columns  => columns(btw_vrij, btw_verlegd)
+                 ,p_hide_null_values => 1) t
+*/
 (
-  p_tab          in dbms_tf.table_t
- ,p_exclude_cols in dbms_tf.columns_t default null
- ,p_date_columns dbms_tf.columns_t default null
+  p_tab              in dbms_tf.table_t
+ ,p_exclude_cols     in dbms_tf.columns_t default null
+ ,p_date_columns     dbms_tf.columns_t default null
+ ,p_boolean_columns  dbms_tf.columns_t default null
+ ,p_hide_null_values in integer default 1
 ) return clob sql_macro is
   l_column_name varchar2(128);
   type key_value_rectype is record(
      k varchar2(128)
     ,v varchar2(128));
   type key_value_tabtype is table of key_value_rectype index by pls_integer;
-  l_key_value_list key_value_tabtype;
-  l_stmt           clob;
-  j                pls_integer := 1;
+  l_key_value_list   key_value_tabtype;
+  l_stmt             clob;
+  j                  pls_integer := 1;
+  l_hide_null_values boolean := (p_hide_null_values = 1);
 begin
   for i in 1 .. p_tab.column.count
   loop
@@ -20,14 +32,17 @@ begin
       -- trim column name
       l_column_name := trim(both '"' from p_tab.column(i).description.name);
     
+      -- camelcase key
       l_key_value_list(j).k := substr(lower(l_column_name), 1, 1) || substr(replace(initcap(l_column_name), '_'), 2);
     
-      if (p_date_columns is not null and p_tab.column(i).description.name member of p_date_columns)
-      then
-        l_key_value_list(j).v := 'to_char(' || p_tab.column(i).description.name || ', ''yyyy-mm-dd''),';
-      else
-        l_key_value_list(j).v := p_tab.column(i).description.name || ',';
-      end if;
+      case
+        when (p_date_columns is not null and p_tab.column(i).description.name member of p_date_columns) then
+          l_key_value_list(j).v := 'to_char(' || p_tab.column(i).description.name || ', ''yyyy-mm-dd''),';
+        when (p_boolean_columns is not null and p_tab.column(i).description.name member of p_boolean_columns) then
+          l_key_value_list(j).v := 'decode(lower('||p_tab.column(i).description.name || '),''j'',''true'', ''ja'',''true'', ''false'') format json,';
+        else
+          l_key_value_list(j).v := p_tab.column(i).description.name || ',';
+      end case;
     
       if (i = p_tab.column.count)
       then
@@ -44,7 +59,12 @@ begin
     l_stmt := l_stmt || '''' || l_key_value_list(i).k || ''':' || l_key_value_list(i).v;
   end loop;
 
-  l_stmt := l_stmt || ' absent on null returning clob) document from p_tab t';
+  if (l_hide_null_values)
+  then
+    l_stmt := l_stmt || ' absent on null returning clob) document from p_tab t';
+  else
+    l_stmt := l_stmt || ' returning clob) document from p_tab t';
+  end if;
 
   dbms_tf.trace(l_stmt);
   return l_stmt;
