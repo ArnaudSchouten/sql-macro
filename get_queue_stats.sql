773697a68ab53310f6e5c070888181e561afd463
Arnaud <100875450+ArnaudSchouten@users.noreply.github.com>
2024-05-22 13:36:38 +0200

    Update get_queue_stats.sql
    add queue name

--- get_queue_stats.sql
+++ get_queue_stats.sql
@@ -1,4 +1,4 @@
-create or replace function get_queue_stats 
+create or replace function get_queue_stats
 return clob 
 sql_macro 
 is
@@ -9,11 +9,11 @@ begin
                   from user_queues t
                  where t.queue_type = 'NORMAL_QUEUE')
   loop
-    l_stmt := l_stmt || 'select q_name queue_name, count(*) aantal from ' || q_rec.queue_table || ' group by q_name union all ';   
+    l_stmt := l_stmt || 'select '''|| q_rec.queue_name ||''' queue_name, case state when 3 then ''Fout'' else ''Oke'' end status, count(*) aantal from ' || q_rec.queue_table || ' group by q_name, state union all ';   
   end loop;
 
   l_stmt := rtrim(l_stmt, 'union all');
-   dbms_tf.trace(l_stmt);
+  dbms_tf.trace(l_stmt);
 
   return(l_stmt);
 end get_queue_stats;
