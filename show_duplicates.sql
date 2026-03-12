d4271d7b978e470c2d1e66e09764befd2a75fbce
Arnaud <100875450+ArnaudSchouten@users.noreply.github.com>
2026-03-12 14:44:56 +0100

    Refactor show_duplicates function parameters and comments
    Updated the show_duplicates function to improve parameter handling and added example usage in comments.

--- show_duplicates.sql
+++ show_duplicates.sql
@@ -1,39 +1,52 @@
-create or replace editionable function show_duplicates (
-	p_tab  in dbms_tf.table_t,
-	p_cols in dbms_tf.columns_t,
-	p_mode in integer default 1
-) return varchar2 sql_macro as
-	l_column_list     varchar2(1000);
-	l_tab_column_list long;
+create or replace function show_duplicates
+(
+  p_tab  in dbms_tf.table_t
+ ,p_cols in dbms_tf.columns_t
+ ,p_mode in dbms_tf.columns_t default null
+) return clob sql_macro as
+  l_column_list     varchar2(1000);
+  l_tab_column_list long;
+  l_stmt            clob;
+  l_mode  pls_integer;
 begin
-	/* p_mode: 1; show duplicate records
-	   p_mode: 2; show rowids of duplicate records
-	   p_mode: 3; show the first record of the duplicate records
-	*/
-	select
-		listagg(column_value,',')
-	  into l_column_list
-	  from table ( p_cols );
+  /* voorbeeld:
+  
+    select *
+    from show_duplicates(p_tab  => lak_import
+                        ,p_cols => columns(loonheffing_nummer, ddstart_periode, ddeind_periode, sv_loon)
+                        ,p_mode => columns("1")
+                        ) t
+  
+     p_mode: 1; show duplicate records
+     p_mode: 2; show rowids of duplicate records
+     p_mode: 3; show the first record of the duplicate records
+  */
+  -- mode
+  if (p_mode.exists(1)) then
+    l_mode := to_number(trim(both '"' from p_mode(1)) default null on conversion error);
+  end if;
+  
+  l_mode := nvl(l_mode, 1);
 
-	for i in 1..p_tab.column.count() loop
-		l_tab_column_list := l_tab_column_list
-		                     || p_tab.column(i).description.name
-		                     || ',';
-	end loop;
+  -- get the columns to compare
+  select listagg(column_value, ',')
+    into l_column_list
+    from table(p_cols);
 
-	l_tab_column_list := rtrim(
-	                          l_tab_column_list,
-	                          ','
-	                     );
-	return replace(
-	              'select '
-	              ||
-			             case
-				             when p_mode = 2 then
-					             'rid'
-				             else l_tab_column_list
-			             end
-	              || '
+  -- get all table columns
+  for i in 1 .. p_tab.column.count()
+  loop
+    l_tab_column_list := l_tab_column_list || p_tab.column(i).description.name || ',';
+  end loop;
+
+  l_tab_column_list := rtrim(l_tab_column_list, ',');
+
+  l_stmt := replace('select ' || case
+                      when l_mode = 2 then
+                       'rid'
+                      else
+                       l_tab_column_list
+                    end || '
             from
                 ( select
                         t.*
@@ -42,18 +55,18 @@ begin
                        ,count(*) over(partition by {l_column_list}) dup_cnt
                     from p_tab t
                 ) t
-            where '
-	              || case
-			             when p_mode = 1 then
-				             ' dup_cnt > 1 '
-			             when p_mode = 2 then
-				             ' dup_cnt > 1 and rn > 1'
-			             when p_mode = 3 then
-				             ' dup_cnt > 1 and rn = 1'
-		             end,
-	              '{l_column_list}',
-	              l_column_list
-	       );
+            where ' || case
+                      when l_mode = 1 then
+                       ' dup_cnt > 1 '
+                      when l_mode = 2 then
+                       ' dup_cnt > 1 and rn > 1'
+                      when l_mode = 3 then
+                       ' dup_cnt > 1 and rn = 1'
+                    end
+                   ,'{l_column_list}'
+                   ,l_column_list);
+
+  return l_stmt;
 
 end show_duplicates;
 /
